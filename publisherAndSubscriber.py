84a2c388b6dc1a0dcbc440385b9931afc73382f6
joao <joaovitorantunes15@hotmail.com>
2024-04-11 15:41:37 -0300

    atualização 2

--- publisherAndSubscriber.py
+++ publisherAndSubscriber.py
@@ -23,46 +23,25 @@ client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 client.connect(mqttBroker) 
 
 def tratarSens():
-    comando = 0
+    comando = ''
     
-    if distance < 100:
+    #tratando da distância da água
+    if distance < 100 and (humidity > 70 and temperature < 20):
         client.loop()
-        client.publish(myTopicActionDistance, '2')
-        print("O nível da água está alto")
+        client.publish(myTopicActionDistance, '1')
+        comando = '1'
+        print("O nível da água está alto! Riscos de alagamento!")
 
-    if distance < 100:
-        client.loop()
-        client.publish(myTopicActionDistance, '3')
-    # Utilizar apenas para testar e vizualizar as informações que estão sendo revebidas do broker.
-    #print("Umidade recebida: ", humidity)
-    #print("Temperatura recebida", temperature)
-    
-    # Se alta umidade e temperatura baixa, então chove
-    if humidity > 70 and temperature < 20:
-        client.loop()
-        client.publish(myTopicAction, '1') # Não ativar alarme
-        print("Provavelmente choverá!")
-    # Se alta umidade e temperatura alta, então chove (chuva forte)
-    elif humidity > 70 and temperature > 25:
+    elif distance < 100 and (humidity > 70 and temperature > 25):
         client.loop()
         client.publish(myTopicAction, '1') # Ativar alarme
-        print("Provavelmente choverá forte!")
-    # Se baixa umidade e temperatura baixa, então não chove
-    elif humidity < 60 and (temperature < 20 or temperature > 25):
-        client.loop()
-        client.publish(myTopicAction, '0') # Não ativar alarme
-        print("Provavelmente não choverá!")
-    # Se baixa umidade e temperatura alta, então não chove 
-    elif humidity < 60 and temperature > 25:
-         client.loop()
-         client.publish(myTopicAction, '0') # Não ativar alarme
-         print("Provavelmente chuva leve!")
+        comando = '1'
+        print("Provavelmente choverá forte! Riscos de alagamento!")
     else:
         client.loop()
         client.publish(myTopicAction, '0') # Não ativar alarme
-        print("Provavelmente chuva leve!")
-
-    time.sleep(3)
+        comando = '0'
+        print("Sem riscos de alagamento.")
 
     return comando
 
@@ -91,50 +70,14 @@ client.subscribe("/Umid/infLed")
 client.message_callback_add("/Dist/infLed", callback)
 client.subscribe("/Dist/infLed")
 
+
 # Aqui haverá as comparações (previsão de chuva), de acordo com as informações nos tópicos do Broker. A partir daí, serão publicados no respectivo tópico as ações a serem tomadas na ESP32
 while True:
     ###### colocar as variaveis de distancia e possibilidade de chuva em um if apenas, apenas se as duas forem True ligar alarme.
-    ###### 
     client.loop()
-
-    #tratando da distância da água0
-    if distance < 100:
-        client.loop()
-        client.publish(myTopicActionDistance, '2')
-        print("O nível da água está alto")
-
-    if distance < 100:
-        client.loop()
-        client.publish(myTopicActionDistance, '3')
-    # Utilizar apenas para testar e vizualizar as informações que estão sendo revebidas do broker.
-    #print("Umidade recebida: ", humidity)
-    #print("Temperatura recebida", temperature)
     
-    # Se alta umidade e temperatura baixa, então chove
-    if humidity > 70 and temperature < 20:
-        client.loop()
-        client.publish(myTopicAction, '1') # Não ativar alarme
-        print("Provavelmente choverá!")
-    # Se alta umidade e temperatura alta, então chove (chuva forte)
-    elif humidity > 70 and temperature > 25:
-        client.loop()
-        client.publish(myTopicAction, '1') # Ativar alarme
-        print("Provavelmente choverá forte!")
-    # Se baixa umidade e temperatura baixa, então não chove
-    elif humidity < 60 and (temperature < 20 or temperature > 25):
-        client.loop()
-        client.publish(myTopicAction, '0') # Não ativar alarme
-        print("Provavelmente não choverá!")
-    # Se baixa umidade e temperatura alta, então não chove 
-    elif humidity < 60 and temperature > 25:
-         client.loop()
-         client.publish(myTopicAction, '0') # Não ativar alarme
-         print("Provavelmente chuva leve!")
-    else:
-        client.loop()
-        client.publish(myTopicAction, '0') # Não ativar alarme
-        print("Provavelmente chuva leve!")
-
+    comando = tratarSens()
+    
     time.sleep(3)
         
         
